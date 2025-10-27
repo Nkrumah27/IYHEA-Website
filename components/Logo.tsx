83da436361b3ad93da94689d575d79ee73d2b9dc
Nkrumah27 <119161107+Nkrumah27@users.noreply.github.com>
2025-10-27 15:54:44 +0000

    made some changes in the header

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,8 +5,7 @@ const Logo: React.FC = () => (
     width="48"
     height="48"
     viewBox="0 0 100 100"
-    /*xmlns="http://www.w3.org/2000/svg"*/
-    src= "/logo.png"
+    xmlns="http://www.w3.org/2000/svg"
     aria-label="IYHEA Logo"
   >
     <defs>
